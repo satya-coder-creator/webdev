bea0c3f972db35950183a4893b6017f57d21cd06
satya-coder-creator <satyavelaga2112@gmail.com>
2025-04-30 20:06:33 +0530

    navigation bar

--- sliderinwebsite/script.js
+++ sliderinwebsite/script.js
@@ -2,11 +2,10 @@ var index = 0;
 function carousel() {
   var i;
   var x = document.getElementsByClassName("myslides");
-
-  for (i = 0; i < x.length; i++) {``
+  for (i = 0; i < x.length; i++)
+  {
     x[i].style.display = "none";
   }
-
   index++;
   if (index > x.length) {
     index = 1;
